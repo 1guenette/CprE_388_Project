abda93d07624d20ebc3bd640dea919e65b9926cf
Leo Freier <lmfreier@iastate.edu>
2018-12-04 17:36:33 -0600

    Adding contacts from QR code. Harcoded information.

--- app/src/main/java/com/example/mrlevick/cpre_388_project/MainActivity.java
+++ app/src/main/java/com/example/mrlevick/cpre_388_project/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mrlevick.cpre_388_project;
 
 import android.Manifest;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.DialogInterface;
@@ -9,6 +11,7 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -26,29 +29,28 @@ import android.widget.Toast;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.journeyapps.barcodescanner.BarcodeResult;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String[] PROJECTION =
-            {
-                    ContactsContract.Data.DISPLAY_NAME,
-                    ContactsContract.CommonDataKinds.Email.DATA,
-                    ContactsContract.CommonDataKinds.Phone.DATA
-            };
     private final int CONTACTS_REQUEST = 55;
     private boolean contactPermission = false;
-    private boolean profileExists = false;
     private TextView mTextMessage;
     public static final int REQUEST_CODE = 100;
     public static final int PERMISSION_REQUEST = 200;
-    private String userPhone = "";
-    private String userEmail = "";
-    private String userWebsite = "";
-    private String userNickname = "";
+    private String userName = "Leo Freier";
+    private String userPhone = "5554446789";
+    private String userEmail = "lmfreier@iastate.edu";
+    private String userWebsite = "leofreier.com";
+    private String userNickname = "Leo";
     private CheckBox boxNumber;
     private CheckBox boxEmail;
     private CheckBox boxWebsite;
     private CheckBox boxNickname;
-    private TextView userName;
+    private TextView userNameText;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -69,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 case R.id.navigation_qrGen:
                     i = new Intent(MainActivity.this, QRgenActivity.class);
-                    i.putExtra("name", userName.getText());
+                    i.putExtra("name", userName);
 
                     if (!boxNumber.isChecked()) {
                         userPhone = "";
@@ -110,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
         boxEmail = findViewById(R.id.email);
         boxWebsite = findViewById(R.id.website);
         boxNickname = findViewById(R.id.nickname);
-        userName = (TextView) findViewById(R.id.userName);
+        userNameText = (TextView) findViewById(R.id.userName);
     }
 
     @Override
@@ -120,56 +122,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onPause(){
+        //save stuff to sharedPreferences
+        super.onPause();
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
             if (data != null) {
                 Barcode barcode = data.getParcelableExtra("qrCode");
                 Log.i("Success", "GOT QR Code: " + barcode.displayValue);
                 Toast.makeText(getApplicationContext(), "GOT QR DATA: " + barcode.displayValue, Toast.LENGTH_SHORT).show();
+                JSONObject readCode = null;
 
-                ContentValues values = new ContentValues();
-                values.put(ContactsContract.RawContacts.ACCOUNT_TYPE, "CprE388");
-                values.put(ContactsContract.RawContacts.ACCOUNT_NAME, "Contact388");
-                Uri contactUri = getContentResolver().insert(ContactsContract.RawContacts.CONTENT_URI, values);
-                long contactId = ContentUris.parseId(contactUri);
-                values.clear();
-                values.put(ContactsContract.Data.RAW_CONTACT_ID, contactId);
-                String contactName, contactPhone, contactEmail, contactWebsite, contactNickname;
+                Intent writeProfileIntent = new Intent(Intent.ACTION_INSERT);
+                writeProfileIntent.setType(ContactsContract.Contacts.CONTENT_TYPE);
+                String contactName = "", contactPhone = "", contactEmail = "", contactWebsite = "", contactNickname = "";
 
-                //TODO I don't think using contactInfo is correct. Need to receive the json.
-                contactName = barcode.contactInfo.name.formattedName;
-                contactPhone = barcode.contactInfo.phones[0].number;
-                contactEmail = barcode.contactInfo.emails[0].address;
-                contactWebsite = barcode.contactInfo.urls[0];
-                //TODO nickname is not in contactInfo.
-                contactNickname = barcode.contactInfo.name.pronunciation;
+                try{
+                    readCode = new JSONObject(barcode.rawValue);
+                    contactName = readCode.getString("name");
+                    System.out.println("Name: " + contactName);
+                    contactPhone = readCode.getString("number");
+                    System.out.println("Phone: " + contactPhone);
+                    contactEmail = readCode.getString("email");
+                    System.out.println("Email: " + contactEmail);
+                    contactWebsite = readCode.getString("website");
+                    Log.i("Website: ", contactWebsite);
+                    contactNickname = readCode.getString("nickname");
+                    System.out.println("Nickname: " + contactNickname);
+                }catch(JSONException e){
+                    Log.i("JSON Failure", "JSONObjet failed to create");
+                }
 
+                ArrayList<ContentValues> otherData = new ArrayList<ContentValues>();
+                ContentValues websiteRow = new ContentValues();
+                ContentValues nicknameRow = new ContentValues();
                 if(!contactName.equals("")) {
-                    values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
-                    values.put(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, userName.getText().toString());
+                    writeProfileIntent.putExtra(ContactsContract.Intents.Insert.NAME, contactName);
                 }
                 if(!contactPhone.equals("")) {
-                    values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
-                    values.put(ContactsContract.CommonDataKinds.Phone.NUMBER, userPhone);
+                    writeProfileIntent.putExtra(ContactsContract.Intents.Insert.PHONE, contactPhone);
                 }
                 if(!contactEmail.equals("")) {
-                    values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE);
-                    values.put(ContactsContract.CommonDataKinds.Email.ADDRESS, userEmail);
+                    writeProfileIntent.putExtra(ContactsContract.Intents.Insert.EMAIL, contactEmail);
                 }
                 if(!contactWebsite.equals("")) {
-                    values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE);
-                    values.put(ContactsContract.CommonDataKinds.Website.URL, userWebsite);
+                    websiteRow.put(ContactsContract.Contacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE);
+                    websiteRow.put(ContactsContract.CommonDataKinds.Website.URL, contactWebsite);
+                    otherData.add(websiteRow);
                 }
                 if(!contactNickname.equals("")) {
-                    values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Nickname.CONTENT_ITEM_TYPE);
-                    values.put(ContactsContract.CommonDataKinds.Nickname.NAME, userNickname);
+                    nicknameRow.put(ContactsContract.Contacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.Nickname.CONTENT_ITEM_TYPE);
+                    nicknameRow.put(ContactsContract.CommonDataKinds.Nickname.NAME, contactNickname);
+                    otherData.add(nicknameRow);
                 }
 
-                getContentResolver().insert(ContactsContract.Data.CONTENT_URI, values);
-
-                Intent writeProfileIntent = new Intent(Intent.ACTION_EDIT);
-                writeProfileIntent.setDataAndType(contactUri, ContactsContract.Contacts.CONTENT_ITEM_TYPE);
-                startActivityForResult(writeProfileIntent, CONTACTS_REQUEST);
+                writeProfileIntent.putParcelableArrayListExtra(ContactsContract.Intents.Insert.DATA, otherData);
+                this.startActivity(writeProfileIntent);
             }
         }
 
@@ -203,101 +214,38 @@ public class MainActivity extends AppCompatActivity {
 
     private void retrieveUserName() {
         String contactName = null;
-        //if(myContactTextView.getText().toString().equals(getString(R.string.startText))) return;
-
-        // querying contact data store
+        String userId = "";
+        // querying profile name
         Cursor cursor = getContentResolver().query(ContactsContract.Profile.CONTENT_URI, null, null, new String[]{}, null);
-
         if (cursor.moveToFirst()) {
-
             contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.Profile.DISPLAY_NAME));
-//            cursor = getContentResolver().query(ContactsContract.Profile.CONTENT_RAW_CONTACTS_URI, null, null, null, null);
-//            //String id = cursor.getString(cursor.getColumnIndex((ContactsContract.RawContacts.CONTACT_ID)));
-//            Uri rawContactUri = ContentUris.withAppendedId(ContactsContract.Profile.CONTENT_RAW_CONTACTS_URI, 0);
-//            Uri entityUri = Uri.withAppendedPath(rawContactUri, ContactsContract.RawContacts.Entity.CONTENT_DIRECTORY);
-//            Cursor phoneCursor = getContentResolver().query(entityUri,
-//                    new String[]{ContactsContract.RawContacts.SOURCE_ID, ContactsContract.RawContacts.Entity.DATA_ID, ContactsContract.RawContacts.Entity.MIMETYPE, ContactsContract.RawContacts.Entity.DATA1},
-//                    null,
-//                    null, null);
-//            while(phoneCursor.moveToNext()){
-//                userPhone = phoneCursor.getString(3);
-//                int type = phoneCursor.getInt(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
-//                switch (type) {
-//                    case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
-//                        // do something with the Home number here...
-//                        break;
-//                    case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
-//                        // do something with the Mobile number here...
-//                        break;
-//                    case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
-//                        // do something with the Work number here...
-//                        break;
-//                }
-//            }
-//            phoneCursor.close();
-//            userEmail = cursor.getString(cursor.getColumnIndex(ContactsContract.RawContacts.Data.DATA1));
-            profileExists = true;
-        } else {
-            //prompt user to create contact
+//            userId = cursor.getString(cursor.getColumnIndex(ContactsContract.Profile.NAME_RAW_CONTACT_ID));
+//            cursor = getContentResolver().query(ContactsContract.RawContacts.CONTENT_URI, null,  null, null, null);
+//            cursor.moveToFirst();
+//            cursor.getString(cursor.getColumnIndex(ContactsContract.RawContacts.Data.DATA1));
+
+        }
+        if(true){
+            //get sharedpreferences
+        }
+        else {
             //tell user a profile must be created
             DialogInterface.OnClickListener createProfileListener = new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     switch (which) {
                         case DialogInterface.BUTTON_POSITIVE:
-                            createProfile();
-                            profileExists = true;
-                            break;
-
-                        case DialogInterface.BUTTON_NEGATIVE:
-                            profileExists = false;
                             break;
                     }
                 }
             };
-
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setMessage("No user profile found. Would you like to create a new one?").setPositiveButton("Yes", createProfileListener)
-                    .setNegativeButton("No", createProfileListener).show();
+            builder.setMessage("No user profile found. Please enter your name manually.").setPositiveButton("OK", createProfileListener).show();
         }
-
         cursor.close();
-
         if (contactName != null) {
-            userName = findViewById(R.id.userName);
-            userName.setText(contactName);
+            userNameText.setText(contactName);
         }
 
-    }
-
-    private void createProfile(){
-        ContentValues values = new ContentValues();
-        Uri contactUri = getContentResolver().insert(ContactsContract.Profile.CONTENT_RAW_CONTACTS_URI, values);
-        long contactId = ContentUris.parseId(contactUri);
-        values.clear();
-        values.put(ContactsContract.Data.RAW_CONTACT_ID, contactId);
-        values.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE);
-        values.put(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, "YourName");
-
-        getContentResolver().insert(ContactsContract.Data.CONTENT_URI, values);
-
-        Intent writeProfileIntent = new Intent(Intent.ACTION_EDIT);
-        writeProfileIntent.setDataAndType(contactUri, ContactsContract.Contacts.CONTENT_ITEM_TYPE);
-
-        startActivityForResult(writeProfileIntent, CONTACTS_REQUEST);
-    }
-
-    public void onEditProfileClick(View v) {
-        if (!contactPermission){
-            requestContactsPermission();
-            return;
-        }
-        if(profileExists)
-            createProfile();
-        else {
-            retrieveUserName();
-            Toast.makeText(this, "Phone is: " + userPhone, Toast.LENGTH_SHORT).show();
-            Toast.makeText(this, "Email is: " + userEmail, Toast.LENGTH_SHORT).show();
-        }
     }
 }
